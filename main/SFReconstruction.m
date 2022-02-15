029c5264fb5313e438ae7a4cce0369822479c22b
Antonio Figueroa DTU <anfig@elektro.dtu.dk>
2022-02-15 17:44:51 +0100

    DOA Implementation v1.0

--- main/SFReconstruction.m
+++ main/SFReconstruction.m
@@ -14,60 +14,61 @@
 clc, clear, close all
 
 addpath(genpath('../tools'))
-%addpath(genpath('/Volumes/anfig/Data/room019/'))
-addpath(genpath('M:\Data'))
+%addpath(genpath('/Volumes/anfig/Data/room019/'))   % MacBook Pro
+addpath(genpath('M:\Data'))                         % Windows 10
 
 loadPlotParams
 
 %% INITIAL PARAMETERS
-T = 5;
-Fs = 48e3;
-D = [6.266,9.357,2.977];    % Room dimensions
+Data.T = 5;
+Data.Fs = 48e3;
+Data.D = [6.266,9.357,2.977];       % Room dimensions
+Data.Nsamples = Data.Fs*Data.T;
+Data.F = [20 20e3];
+Data.f = linspace(Data.F(1),Data.F(2),Data.Nsamples);
 
-time = [5e-3,25e-3];   % Plot lenght
-Nsamples = Fs*T;
-Nplot = Fs*time;
-
-loudspeaker = 'rir_019_spk1.h5';
+Plot.T = [5 25]*1e-3;            % Plot lenght
+Plot.Nsamples = Data.Fs*Plot.T;
+Data.loudspeaker = 'rir_019_spk1.h5';
 
 %% DATA ACQUISITION
 % Line 1
-pos1 = h5read(loudspeaker,'/dataset_lin2/position');
-h1 = h5read(loudspeaker,'/dataset_lin2/impulse_response');
+Data.Line1.pos = h5read(Data.loudspeaker,'/dataset_lin2/position');
+Data.Line1.h = h5read(Data.loudspeaker,'/dataset_lin2/impulse_response');
 
 % Line 2
-pos2 = h5read(loudspeaker,'/dataset_lin1/position');
-h2 = h5read(loudspeaker,'/dataset_lin1/impulse_response');
+Data.Line2.pos = h5read(Data.loudspeaker,'/dataset_lin1/position');
+Data.Line2.h = h5read(Data.loudspeaker,'/dataset_lin1/impulse_response');
 
 % Line 3
-pos3 = h5read(loudspeaker,'/dataset_lin3/position');
-h3 = h5read(loudspeaker,'/dataset_lin3/impulse_response');
+Data.Line3.pos = h5read(Data.loudspeaker,'/dataset_lin3/position');
+Data.Line3.h = h5read(Data.loudspeaker,'/dataset_lin3/impulse_response');
 
 % Sphere
-posSph = h5read(loudspeaker,'/dataset_sph1/position');
-hSph = h5read(loudspeaker,'/dataset_sph1/impulse_response');
+Data.Sph.pos = h5read(Data.loudspeaker,'/dataset_sph1/position');
+Data.Sph.h = h5read(Data.loudspeaker,'/dataset_sph1/impulse_response');
 
 %% DATA MERGING
-h_ref = horzcat(h1,h2,h3);
-posLin = vertcat(pos1,pos2,pos3);
+Data.Ref.h = horzcat(Data.Line1.h,Data.Line2.h,Data.Line3.h);
+Data.Ref.pos = vertcat(Data.Line1.pos,Data.Line2.pos,Data.Line3.pos);
 
-% Inner Sphere (samples 155-end)
-posInnSph = posSph(155:end,:);
-hInnSph = hSph(:,155:end);
+% Inner Sphere (156 Microphones: samples 155-end)
+Data.InnSph.h = Data.Sph.h(:,155:end);
+Data.InnSph.pos = Data.Sph.pos(155:end,:);
 
 %% SETUP PLOT
 % Time vector
-t = time(1):1/Fs:time(2)-(1/Fs);
+Plot.t = Plot.T(1):1/Data.Fs:Plot.T(2)-(1/Data.Fs);
 
 % Data downsizing
-href2 = h_ref(Nplot(1):Nplot(2)-1,:);
-hInnSph2 = hInnSph(Nplot(1):Nplot(2)-1,:);
+Plot.Ref.h = Data.Ref.h(Plot.Nsamples(1):Plot.Nsamples(2)-1,:);
+Plot.InnSph.h = Data.InnSph.h(Plot.Nsamples(1):Plot.Nsamples(2)-1,:);
 
 % Line plot
 figure
-scatter3(posLin(:,1),posLin(:,2),posLin(:,3)), hold on
-scatter3(posInnSph(:,1),posInnSph(:,2),posInnSph(:,3))
-axis([0 D(1) 0 D(2) 0 D(3)])
+scatter3(Data.Ref.pos(:,1),Data.Ref.pos(:,2),Data.Ref.pos(:,3)), hold on
+scatter3(Data.InnSph.pos(:,1),Data.InnSph.pos(:,2),Data.InnSph.pos(:,3))
+axis([0 Data.D(1) 0 Data.D(2) 0 Data.D(3)])
 xlabel('x in m'), ylabel('y in m'), zlabel('z in m')
 legend('Reference Line','Spherical Array')
 applyAxisProperties(gca)
@@ -75,9 +76,9 @@ applyLegendProperties(gca)
 
 %% REFERENCE LINE RIR PLOT
 figure
-s = surf(posLin(:,1),t,href2);
+s = surf(Data.Ref.pos(:,1),Plot.t*1e3,Plot.Ref.h);
 set(s,'edgecolor','none')
-xlabel('x in m'), ylabel('y in m')
+xlabel('x in m'), ylabel('Time in ms')
 colormap hot
 view(2)
 c = colorbar;
@@ -85,10 +86,16 @@ caxis([-0.04 0.04])
 applyColorbarProperties(c,'Room Impulse Response in Pa/V')
 applyAxisProperties(gca)
 
-
 %% DIRECT SOUND FIELD
-% Time window: 0-50 ms
+% Time window: 5-10 ms
+Direct.T = [5 10]*1e-3;
+Direct.Nsamples = Data.Fs*Direct.T;
 
+% Data Downsizing
+Direct.InnSph.h = Data.InnSph.h(Direct.Nsamples(1):Direct.Nsamples(2)-1,:);
 
+% DOA Estimation
+DOA.N = 500;        % Number of plane waves
+[DOA.H,DOA.uk] = dictionary(Data.f,Data.InnSph.pos',DOA.N);
 
 
--- tools/somp.m
+++ tools/somp.m
@@ -1,11 +1,11 @@
 function x = somp(H, p, K)
 %x = somp(CA, b, k) Solve the sparse coding problem via SOMP
-%   min_x sum(||b - CAx||_2^2) s.t. ||x||_0 <= K
+%   min_x sum(||p - CAx||_2^2) s.t. ||x||_0 <= K
 %   
 %   Input:
 %       - H         : dictionary. M x N x Nf
 %       - p         : measurements. M x Nf
-%       - k         : number of possible sources. Scalar
+%       - K         : number of possible sources. Scalar
 %   Output:
 %       - x         : estimated beamformer output. N x L
 %
@@ -13,7 +13,7 @@ function x = somp(H, p, K)
 % Date: February 2022
 
 H = H./vecnorm(H,2,2);  % Normalise columns
-[~,N,Nf] = size(H);      % Dictionary dimentions
+[~,N,Nf] = size(H);     % Dictionary dimentions
 
 s = zeros(K,1);         % Support
 r = p;                  % Initialise residual
